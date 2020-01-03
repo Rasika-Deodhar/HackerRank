daf8f9308cfd2f5403f4aeeab60d4d433bc3b890
Rasika-Deodhar <rasikadeodhar95@gmail.com>
2020-01-03 02:17:45 +0000

    Magazine and Note

---
+++ Magazine and Note/Solution.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+
+        int flag = 0, count=0;
+
+        for (int i = 0; i < note.length; i++) {
+            for (int j = 0; j < magazine.length; j++) {
+                flag = 0; count++;
+                if (note[i].equals(magazine[j])) {
+                    magazine[j] = " ";
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0 && count==magazine.length){
+                System.out.println("No");
+                break;
+            }
+            count=0;
+        }
+        if(flag!=0){
+            System.out.println("Yes");
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
