fd7d2469beb7f68999a30ea1d252e89ba49f86f8
Rasika Deodhar <rasikadeodhar95@gmail.com>
2020-12-06 23:28:45 +0000

    solved old unsolved sequence problem

--- Dynamic Arrays - Sequence/Solution.java
+++ Dynamic Arrays - Sequence/Solution.java
@@ -1,15 +1,11 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Result {
 
@@ -21,36 +17,23 @@ class Result {
      */
 
     public static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
-        // Write your code here
 
         List<ArrayList<Integer>> seqList = new ArrayList<ArrayList<Integer>>(n);
         List<Integer> output = new ArrayList<Integer>();
         int lastAnswer = 0;
 
-        for (List<Integer> list : queries) {
-            // int index = Xor(list.get(1), lastAnswer) % n;
-            int index = (list.get(1) ^ lastAnswer) % n;
-            if (list.get(0) == 1) {
-                if (seqList.isEmpty()) {
-                    seqList.add(new ArrayList<Integer>());
-                    seqList.get(0).add(list.get(2));
-                } else {
-                    try {
-                        seqList.get(index);
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                        seqList.add(new ArrayList<Integer>());
-                        seqList.get(index).add(list.get(2));
-                    }
-                    // seqList.get(index).add(list.get(2));// for seqList index=1, make a list then
-                    // add element
-                }
+        for (int i = 0; i < n; i++) {
+            seqList.add(new ArrayList<Integer>());
+        }
 
-            } else {
-                ArrayList<Integer> seq = seqList.get(index);
-                lastAnswer = (list.get(2) % seq.size());
-                System.out.println(lastAnswer);
+        for (List<Integer> list : queries) {
+            if (list.get(0) == Integer.valueOf(1)) {
+                seqList.get((list.get(1) ^ lastAnswer) % n).add(list.get(2));
+            } else if (list.get(0) == Integer.valueOf(2)) {
+                ArrayList<Integer> seq = seqList.get((list.get(1) ^ lastAnswer) % n);
+                lastAnswer = seq.get(list.get(2) % seq.size());
                 output.add(lastAnswer);
+                // System.out.println(lastAnswer);
             }
         }
 
@@ -62,31 +45,27 @@ class Result {
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(firstMultipleInput[0]);
+        String str = sc.nextLine();
+        String[] str_ = str.split(" ");
 
-        int q = Integer.parseInt(firstMultipleInput[1]);
+        int n = Integer.valueOf(str_[0]).intValue();
+        int q = Integer.valueOf(str_[1]).intValue();
 
         List<List<Integer>> queries = new ArrayList<>();
 
         IntStream.range(0, q).forEach(i -> {
-            try {
-                queries.add(Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt).collect(toList()));
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
+            queries.add(Stream.of(sc.nextLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt)
+                    .collect(toList()));
         });
 
         List<Integer> result = Result.dynamicArray(n, queries);
+        for (Integer integer : result) {
+            System.out.println(integer);
+        }
 
-        bufferedWriter.write(result.stream().map(Object::toString).collect(joining("\n")) + "\n");
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        sc.close();
     }
 }
