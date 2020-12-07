a2bdb069130059a5a7bf85b79282e4b19f730371
Rasika Deodhar <rasikadeodhar95@gmail.com>
2020-12-07 18:23:06 +0000

    Array Manipulation problem solved using Discussion approach

--- Array Manipulation/Solution.java
+++ Array Manipulation/Solution.java
@@ -1,42 +1,68 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Solution {
 
     // Complete the arrayManipulation function below.
     static long arrayManipulation(int n, int[][] queries, int m) {
 
+        // Non-optimized solution
+        // long[] a = new long[n];
+        // for(int i=0; i<n;i++){
+        // a[i] = 0;
+        // }
+
+        // for(int i=0; i<m; i++){
+        // for(int j=(queries[i][0]-1); j<=(queries[i][1]-1); j++){
+        // a[j] += queries[i][2];
+        // }
+        // }
+
+        // long indexArrayCount=0;
+        // for(int i=0; i < n; i++){
+        // if(a[i] > indexArrayCount){
+        // indexArrayCount = a[i];
+        // }
+        // System.out.print(a[i] + " ");
+        // }
+
+        // return indexArrayCount;
+
+        // Another approach
         long[] a = new long[n];
-        for(int i=0; i<n;i++){
-            a[i] = 0;
-        }
-        
-        for(int i=0; i<m; i++){
-            for(int j=(queries[i][0]-1); j<=(queries[i][1]-1); j++){
-                a[j] += queries[i][2];
-            }
+        long max = 0, sum = 0;
+
+        for (int i = 0; i < m; i++) {
+
+            //Similar - unoptimized
+            // for (int j = queries[i][0] - 1; j < queries[i][1]; j++) {
+            // a[j] += queries[i][2];
+            // if (a[j] > max) {
+            // max = a[j];
+            // }
+            // }
+
+            // Rodney Shag's approach:
+            //Add k to left boundary, subtract k in right boundary, nothing in between values
+            a[queries[i][0] - 1] += queries[i][2];
+            a[queries[i][1]] -= queries[i][2];
         }
 
-        long indexArrayCount=0;
-        for(int i=0; i < n; i++){
-            if(a[i] > indexArrayCount){
-                indexArrayCount = a[i];
-            }
-            System.out.print(a[i] + " ");
+        //find sum of array and check if max value is reached.
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+            if (max < sum)
+                max = sum;
         }
-    
-        return indexArrayCount;
+
+        return max;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new
+        // FileWriter(System.getenv("OUTPUT_PATH")));
 
         String[] nm = scanner.nextLine().split(" ");
 
